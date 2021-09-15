f22dd6567e8078469052ca68b9f78502a9ac31d3
AlessioDPT <alessio.recos@gmail.com>
2021-09-15 18:14:38 +0200

    ADD- category in Create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -10,7 +10,17 @@
           <label for="titolo" class="form-label">Titolo</label>
           <input type="text" class="form-control" id="titolo" aria-describedby="emailHelp" name="title">
           <label for="descrizione" class="form-label">articolo</label>
-          <textarea name="article" id="descrizione" cols="30" rows="10" class="form-control"></textarea>  
+          <textarea name="article" id="descrizione" cols="30" rows="10" class="form-control"></textarea>
+          <label for="cat" class="form-label">categoria</label>
+          <select name="category_id" id="cat">
+            <option value="">Seleziona una categoria</option>
+            @foreach ($categories as $category)
+              <option value="{{$category->id}}"
+                @if ($category->id == old('category_id')) selected
+                @endif
+                >{{$category->name}}</option>
+            @endforeach
+          </select>
         </div>
         <button type="submit" class="btn btn-primary">crea</button>
       </form>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -11,7 +11,7 @@
         <input type="text" class="form-control" id="titolo" aria-describedby="emailHelp" name="title" value="{{ old('title', $post->title)}}">
         <label for="descrizione" class="form-label">articolo</label>
         <textarea name="article" id="descrizione" cols="30" rows="10" class="form-control">{{ old('article', $post->article)}}</textarea>
-        <label for="cat" class="form-label">articolo</label>
+        <label for="cat" class="form-label">categoria</label>
         <select name="category_id" id="cat" class="form-control">
             <option value="">Seleziona una categoria</option>
             @foreach ($categories as $category)
